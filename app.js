a9d4774a4fce065dbc44dbf0b855f2e572836ee2
Jeffrey <okoobohefosa@gmail.com>
2020-04-01 22:52:26 +0100

    add db and collection creation with README.md images

---
+++ app.js
@@ -0,0 +1,10 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/fosajeff';
+ 
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    const my_db = db.db("fosajeff");
+    console.log(`Database created by ${my_db.databaseName}`)
+    db.close()
+});
+
